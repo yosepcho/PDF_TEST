0328dfa149a1496c955bb5b2031b144b7a7e7f03
Yosep Cho <joyoseb@yonsei.ac.kr>
2026-09-21 16:07:33 +0900

    6 debug

--- main.py
+++ main.py
@@ -3,9 +3,20 @@ from kivy.uix.label import Label
 
 try:
     from smb.SMBConnection import SMBConnection
-    txt = "Import OK"
+    conn = SMBConnection(
+        "ldr",
+        "Dustpno1!",
+        "Tablet",
+        "10.29.10.40",
+        use_ntlm_v2=True
+    ) 
+    connected = conn.connect("10.29.10.40", 445)
+    txt = str(connected)
+
 except Exception as e:
     txt = str(e)
+
+
 
 class TestApp(App):
     def build(self):
